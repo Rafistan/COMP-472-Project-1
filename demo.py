50e77196412a0c36026e1169171a34686ae58f78
Rafi <rafistep98@gmail.com>
2022-10-10 01:32:15 -0400

    Sector 1

--- demo.py
+++ demo.py
@@ -1,5 +1,10 @@
 import json
 from typing import Dict, List
+import matplotlib
+
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def readFile():
@@ -15,13 +20,20 @@ def readFile():
                 emotions.append(redditPost[1])
                 postsCategorizedByEmotions[redditPost[1]] = [redditPost[0]]
             else:
-                postsCategorizedByEmotions[redditPost[2]].append(redditPost[0])
+                postsCategorizedByEmotions[redditPost[1]].append(redditPost[0])
 
             if redditPost[2] not in sentiments:
                 sentiments.append(redditPost[2])
                 postsCategorizedBySentiments[redditPost[2]] = [redditPost[0]]
             else:
                 postsCategorizedBySentiments[redditPost[2]].append(redditPost[0])
+        return [postsCategorizedBySentiments, postsCategorizedByEmotions]
 
 
-readFile()
+[sentiments, emotions] = readFile()
+sentimentKeys = sentiments.keys()
+emotionKeys = emotions.keys()
+plt.pie(np.array([len(attr) for attr in sentimentKeys]), labels=list(sentiments), autopct='%1.1f%%')
+plt.show()
+plt.pie(np.array([len(attr) for attr in emotionKeys]), labels=list(emotions), autopct='%1.1f%%')
+plt.show()
