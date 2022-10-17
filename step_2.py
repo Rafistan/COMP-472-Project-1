e13801f9cd3cdc8f39da342250bc3643c9344360
GAMPR <tigran1918@gmail.com>
2022-10-16 21:40:17 -0400

    tried train_test_split

--- step_2.py
+++ step_2.py
@@ -1,5 +1,6 @@
 from sklearn.feature_extraction.text import CountVectorizer
 import json
+from sklearn.model_selection import train_test_split 
 
 with open('./goemotions.json', 'r') as file:
     redditPosts = json.load(file)
@@ -7,3 +8,8 @@ with open('./goemotions.json', 'r') as file:
     vectorizer = CountVectorizer()
     X = vectorizer.fit_transform(descriptions)
     print(vectorizer.get_feature_names_out())
+
+    #dikran try train_test_split, no idea if it worked 
+    x_train, x_test = train_test_split(X, test_size=0.2)
+    print('xtrain', x_train) 
+    print('x_test', x_test) 
